db7b3dee3d029005264c7bb7bbd9001139f06296
Yadav, Indal Kumar (US - Hyderabad) <indal.kumar010@gmail.com>
2021-04-17 11:14:11 +0530

    Day21 content

---
+++ Day21_CSVHelperAndJson/Day21_CSVHelperAndJson/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day21_CSVHelperAndJson
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("# Read data from CSV & Write data in CSV");
+            //CSVHandler.ImplementCSVDataHandling();
+            ReadCSV_And_WriteJSON.ImplementCSVToJSON();
+
+            Console.ReadKey();
+        }
+    }
+}
