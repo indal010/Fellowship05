ed8ba0f54800a6e2b93b91e78ee00189f6a1bb9f
Yadav, Indal Kumar (US - Hyderabad) <indal.kumar010@gmail.com>
2021-04-20 07:53:48 +0530

    Day 28 content

---
+++ LinqToDataTableDemo/LinqToDataTableDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinqToDataTableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Table demo");
+            LinqToDataTable linqToDataTable = new LinqToDataTable();
+            linqToDataTable.AddToDataTableDemo();
+            Console.ReadKey();
+        }
+    }
+}
